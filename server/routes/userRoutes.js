1abb77c00df64862481835f53f10f7420ccc0b85
BiancaBaicoianu <95134928+BiancaBaicoianu@users.noreply.github.com>
2024-01-03 23:12:15 +0200

    encryption user data + login user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,4 +9,15 @@ router.put('/update/:id', userController.updateUser);
 
 router.delete('/delete/:id', userController.deleteUser);    
 
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+      const loginResult = await userController.loginUser(username, password);
+      res.status(200).json(loginResult);
+    } catch (err) {
+      res.status(401).json({ success: false, message: err.message });
+    }
+  });
+
 module.exports = router;
