2cba847350e786596e26e34956b9b19033151ed8
Simona <simona@pop-os.localdomain>
2024-01-10 14:09:43 +0200

    History functionality

--- server/controllers/historyController.js
+++ server/controllers/historyController.js
@@ -1,10 +1,16 @@
 const {History} = require('../models/historyModel');
-
+const {Insect} = require('../models/insectModel');
 
 const getHistory = async (req, res) => {
 
     try{
-        const history = await History.find({user: req.params.id}).populate('insect');
+        if (!req.user || !req.user.id) {
+            return res.status(403).json({ message: 'Acces interzis - Utilizatorul nu este autentificat' });
+          }
+      
+        console.log(req.user);
+        const userId = req.user.id;
+        const history = await History.find({ user: userId }).populate('insect');
         res.status(200).json({
             success: true,
             data: history
@@ -18,4 +24,28 @@ const getHistory = async (req, res) => {
     }
 };
 
-module.exports = {getHistory};
+const addHistory = async (req, res) => {
+    
+        try{
+            if (!req.user || !req.user.id) {
+                return res.status(403).json({ message: 'Acces interzis - Utilizatorul nu este autentificat' });
+            }
+          
+            const userId = req.user.id;
+            req.body.user = userId;
+
+            const history = await History.create(req.body);
+            res.status(200).json({
+                success: true,
+                data: history
+            });
+        }
+        catch(err){
+            res.status(400).json({
+                success: false,
+                message: err.message
+            });
+        }
+};
+
+module.exports = {getHistory, addHistory};
