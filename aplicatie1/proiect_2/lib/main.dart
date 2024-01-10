61bfaa13eb61b42bcec58e304e71b45f4fc82565
Lem-Rau Dumitru Alexandru <93475691+DumilexMan@users.noreply.github.com>
2024-01-11 01:45:10 +0200

    Update main.dart
    Adaugare pagini
    Adaugare functionalitate camera

--- aplicatie1/proiect_2/lib/main.dart
+++ aplicatie1/proiect_2/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
@@ -51,6 +54,20 @@ class HomePage extends StatelessWidget {
                 MaterialPageRoute(builder: (context) => AccountPage()),
               );
             }),
+            const SizedBox(height: 16),
+            MenuItemButton('Login', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            }),
+            const SizedBox(height: 16),
+            MenuItemButton('Signup', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SignupPage()),
+              );
+            }),
           ],
         ),
       ),
@@ -73,7 +90,36 @@ class MenuItemButton extends StatelessWidget {
   }
 }
 
-class TakePhotoPage extends StatelessWidget {
+class TakePhotoPage extends StatefulWidget {
+  @override
+  _TakePhotoPageState createState() => _TakePhotoPageState();
+}
+
+class _TakePhotoPageState extends State<TakePhotoPage> {
+  File? _selectedImage;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+    }
+  }
+
+  Future<void> _takePhoto() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.camera);
+
+    if (pickedImage != null) {
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,10 +127,10 @@ class TakePhotoPage extends StatelessWidget {
         title: const Text('Take Photo Page'),
       ),
       body: Align(
-        alignment: const Alignment(0.9, 0.9),
+        alignment: const Alignment(0.9, 0.9), // Aliniere în partea dreaptă jos pentru primul buton
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end, // Ajustează alinierea pentru a fi în partea dreaptă
           children: [
             ElevatedButton(
               onPressed: () {
@@ -98,18 +144,32 @@ class TakePhotoPage extends StatelessWidget {
               ),
               child: const Icon(Icons.home, color: Color.fromARGB(255, 255, 255, 255)),
             ),
-            SizedBox(height: 16), // Adaugă un spațiu între butoane (poți ajusta valoarea la nevoie)
+          ],
+        ),
+      ),
+      bottomNavigationBar: Align(
+        //alignment: const Alignment(0, 0.9), // Aliniere în mijloc jos pentru al doilea buton
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
             ElevatedButton(
-              onPressed: () {
-                // Acțiune pentru al doilea buton
-                // Poți adăuga logica dorită aici
-              },
+              onPressed: _pickImage,
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.all(16),
                 shape: CircleBorder(),
                 backgroundColor: Colors.green, // Culoarea pentru al doilea buton
               ),
-              child: const Icon(Icons.add, color: Color.fromARGB(255, 255, 255, 255)), // Iconița pentru al doilea buton
+              child: const Icon(Icons.photo, color: Color.fromARGB(255, 255, 255, 255)), // Iconița pentru al doilea buton
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _takePhoto,
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(16),
+                shape: CircleBorder(),
+                backgroundColor: Colors.orange, // Culoarea pentru butonul de a lua fotografii
+              ),
+              child: const Icon(Icons.camera, color: Color.fromARGB(255, 255, 255, 255)), // Iconița pentru butonul de a lua fotografii
             ),
           ],
         ),
@@ -144,25 +204,27 @@ class DashboardPage extends StatelessWidget {
 }
 
 class HistoryPage extends StatelessWidget {
+  final List<String> items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('History Page'),
       ),
-      body: Align(
-        alignment: const Alignment(0.9, 0.9),
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.all(16),
-            shape: CircleBorder(),
-            backgroundColor: Colors.blue,
-          ),
-          child: const Icon(Icons.home, color: Color.fromARGB(255, 255, 255, 255),),
-        ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+          );
+        },
       ),
     );
   }
@@ -192,3 +254,105 @@ class AccountPage extends StatelessWidget {
     );
   }
 }
+
+
+// Login page
+
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Adaugă logica de autentificare aici
+                String username = usernameController.text;
+                String password = passwordController.text;
+                print('Username: $username, Password: $password');
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignupPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Signup Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Adaugă logica de înregistrare aici
+                String username = usernameController.text;
+                String password = passwordController.text;
+                String email = emailController.text;
+                print('Username: $username, Password: $password, Email: $email');
+              },
+              child: Text('Signup'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
